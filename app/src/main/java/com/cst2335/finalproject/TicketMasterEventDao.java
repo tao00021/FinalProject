428fc6258accbe0e5bf48eb4eb8029cd17b175a2
Ran Tao <tao00021@algonquinlive.com>
2022-04-02 17:06:43 -0400

    Database and ticketMaster web service -Ran_Tao

---
+++ app/src/main/java/com/cst2335/finalproject/TicketMasterEventDao.java
@@ -0,0 +1,25 @@
+package com.cst2335.finalproject;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TicketMasterEventDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void addEventToDB(TicketMasterEvent event);
+
+    @Query("select * from TicketMasterEvent")
+    List<TicketMasterEvent> getAllEvents();
+
+    @Query("select * from TicketMasterEvent where city = :eventCity")
+    List<TicketMasterEvent> getAllEventsFromCity(String eventCity);
+
+    @Query("delete from TicketMasterEvent where city = :eventCity")
+    void removeEventsFromCity(String eventCity);
+}
